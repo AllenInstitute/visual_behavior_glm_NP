aad96f368671ba33831b0e42318735b54e888cc4
Alex Piet <alexpiet@gmail.com>
2023-05-17 16:40:57 -0700

    updating setup

--- setup.py
+++ setup.py
@@ -1,24 +1,12 @@
 from setuptools import setup
 
-setup(name='visual-behavior-glm',
-      packages=['visual_behavior_glm'],
-      version='0.0.1',
+setup(name='visual-behavior-glm-np',
+      packages=['visual_behavior_glm_NP'],
+      version='1.0.0',
       description='GLM for Visual Behavior ophys data',
       url='https://github.com/AllenInstitute/visual_behavior_glm',
       author='Alex Piet, Nick Ponvert, Doug Ollerenshaw, Marina Garrett',
       author_email="alex.piet@alleninstitute.org, nick.ponvert@alleninstitute.org, dougo@alleninstitute.org, marinag@alleninstitute.org",
       license='Allen Institute',
-      dependency_links=['https://github.com/AllenInstitute/visual_behavior_analysis.git'],
-      install_requires=[
-        "allensdk",
-        "h5py",
-        "matplotlib",
-        "plotly",
-        "pandas==0.25.3",
-        "seaborn",
-        "numpy",
-        "pymongo",
-        "xarray==0.15.1",
-        "xarray_mongodb",
-      ],
+      #dependency_links=['https://github.com/AllenInstitute/visual_behavior_analysis.git'],
      )
